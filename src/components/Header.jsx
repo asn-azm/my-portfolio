c17b8f50ab8821d998fe3d8b44700da84ff12cf8
Ahsan Azam <aazam@teresol.com>
2025-01-12 15:28:12 +0500

    Trying to display profile image and cv

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,7 +37,7 @@ const Header = (props) => {
             <a href="#blog">Blog</a>
           </li>
           <li>
-            <a href="public/AhsanAzamCV.pdf" target="_blank" rel="noopener noreferrer">
+            <a href="/AhsanAzamCV.pdf" target="_blank" rel="noopener noreferrer">
               View My CV
             </a>
           </li>
