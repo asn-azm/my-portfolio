a98e5c5a3fb129d5b6890e70095e2407617f7049
Ahsan Azam <aazam@teresol.com>
2025-01-24 11:44:41 +0500

    jobs-app is added also the git url and the live demo is added in this portfolio app

--- public/data/ProjectData.js
+++ public/data/ProjectData.js
@@ -1,13 +1,13 @@
 export const ProjectList = [
   {
-    img: "/project2.png",
+    img: "/p1test1.png",
     name:"to be updated",
-    title: "YouKnowTheEnding",
+    title: "Work Vibe",
     description:
-      "Web development focuses on creating websites using technologies like HTML, CSS, JavaScript, and frameworks such as React and Node.js to build responsive and dynamic applications.",
-    tech_stack: ["React Native", "Google Cloud", "NextJs", "TailwindCSS"],
-    github_url: "https://github.com/asnazm",
-    demo_url: "https://github.com/asnazm",
+      "The application is built with React for the front-end, utilizing Material-UI (MUI) components and custom CSS for styling. The back-end is developed using Node.js and Express.js, with MongoDB serving as the NoSQL database for efficient data storage.",
+    tech_stack: ["React", "MUI", "Node", "Express", "Mongodb"],
+    github_url: "https://github.com/asn-azm/jobs-app",
+    demo_url: "https://jobs-app-tau.vercel.app/",
   },
   {
     img: "/project1.jpg",
